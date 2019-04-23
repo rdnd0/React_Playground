b611986b5bf25c7b3cd18d5dbe9f48d247c84ad9
David Redondo <rdnd201@gmail.com>
2019-04-23 15:13:25 +0200

    context api basic example

--- src/App.js
+++ src/App.js
@@ -1,30 +1,62 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Toggle from "./components/Toggle";
+import User from "./components/User";
+import { UserContext } from "./components/UserContext";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Toggle>
-          {({ on, toggle }) => (
+class UserProvider extends Component {
+  state = {
+    name: "Dave",
+    surname: "Round",
+    email: "dave@round.com"
+  };
+
+  logout = () => {
+    this.setState({
+      name: "",
+      surname: "",
+      email: ""
+    });
+  };
+  render() {
+    return (
+      <UserContext.Provider value={{ user: this.state, logout: this.logout }}>
+        {this.props.children}
+      </UserContext.Provider>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <UserProvider>
+        <div className="App">
+          <header className="App-header">
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
             <div>
-              {on && <h1>Toggle on!</h1>}
-              <button onClick={toggle}>show/hide</button>
+              <User />
+              <Toggle>
+                {({ on, toggle }) => (
+                  <div>
+                    {on && <h1>Toggle on!</h1>}
+                    <button onClick={toggle}>show/hide</button>
+                  </div>
+                )}
+              </Toggle>
             </div>
-          )}
-        </Toggle>
-      </header>
-    </div>
-  );
+          </header>
+        </div>
+      </UserProvider>
+    );
+  }
 }
 
 export default App;
